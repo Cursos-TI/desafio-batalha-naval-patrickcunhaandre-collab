6f8503847f857eb2d3588a274efc20bedcf93942
Patrick André <patrick.cunha.andre@gmail.com>
2026-06-24 00:15:41 -0300

    Implementado escolhas para o jogador
    Implementado sistema de menus e escalabilidade para tabuleiros dinamicos.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,68 +1,204 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <windows.h>
 
-void impressaotabuleiro(int tabuleiro[10][10], char colunas[10]);
+#define LINHAS 10
+#define COLUNAS 10
+#define AGUA 0
+#define NAVIO 3
+
+void impressaotabuleiro(int tabuleiro[LINHAS][COLUNAS]);
+void shipdrop(int tabuleiro[LINHAS][COLUNAS]);
+void limparTela() {
+        printf("\033[H\033[J"); // Limpa a tela para manter a interface amigável
+}
 
 int main (){
-
-    char colunas[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int tabuleiro[10][10];
-    int posnavioX, posnavioY;
-    
-    //Inicializa o tabuleiro com apenas água
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
-        }
+    limparTela();
+    printf(
+    "===========================================================================\n"
+    "                                                                           \n"
+    "        ██████╗  █████╗ ████████╗ ██████╗  ██╗     ██╗   ██╗   ██████╗     \n"
+    "        ██╔══██╗██╔══██╗╚══██╔══╝██╔═══██╗ ██║     ██║   ██║  ██║   ██║    \n"
+    "        ██████╔╝███████║   ██║   ████████║ ██║     ████████║  ████████║    \n"
+    "        ██╔══██╗██╔══██║   ██║   ██║   ██║ ██║     ██║   ██║  ██║   ██║    \n"
+    "        ██████╔╝██║  ██║   ██║   ██║   ██║ ██████╗ ██║   ██║  ██║   ██║    \n"
+    "        ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝   ╚═╝  ╚═╝   ╚═╝    \n"
+    "                                                                           \n"
+    "        ███╗   ██╗ █████╗ ██╗   ██╗ █████╗  ██╗                            \n"
+    "        ████╗  ██║██╔══██╗██║   ██║██╔══██╗ ██║                            \n"
+    "        ██╔██╗ ██║███████║██║   ██║███████║ ██║                            \n"
+    "        ██║╚██╗██║██╔══██║╚██╗ ██╔╝██╔══██║ ██║                            \n"
+    "        ██║ ╚████║██║  ██║ ╚████╔╝ ██║  ██║ ██████╗                        \n"
+    "        ╚═╝  ╚═══╝╚═╝  ╚═╝  ╚═══╝  ╚═╝  ╚═╝ ╚═════╝    by Patrick Andre    \n"
+    "                                                                           \n"
+    "===========================================================================\n"
+    );
+    //Falsa tela de loading para experiencia do usuário
+        printf("      Carregando ");
+    for (int i = 1; i < 40; ++i){
+        Sleep(100);
+        printf("█");
     }
-    impressaotabuleiro(tabuleiro, colunas);
+    Sleep(1000);
 
-    // posicionar o primeiro navio
+    int menu;
+    
+    do {
+        limparTela();
+        printf("Escolha uma opção do menu\n"
+                "1- Jogar\n"
+                "2- Sair\n"
+            );
+        do {
+            scanf("%i", &menu);
+            if (menu < 1 || menu > 2 || menu == 'A') {
+                printf("Opção inválida. Digite novamente: ");
+            }
+        } while (menu < 1 || menu > 2 || menu == 'A');
+        
+        switch (menu)
+        {
+        case 1:
+            //Inicio do jogo
+            int tabuleiro[LINHAS][COLUNAS];
+            limparTela();
+            //Inicializa o tabuleiro com apenas água
+            for (int i = 0; i < LINHAS; i++) {
+                for (int j = 0; j < COLUNAS; j++) {
+                    tabuleiro[i][j] = AGUA;
+                }
+            }
+            printf("Seu tabuleiro de jogo:\n\n");
+            impressaotabuleiro(tabuleiro);
+            
+            shipdrop(tabuleiro);
+            limparTela();
+            
+            impressaotabuleiro(tabuleiro);
+            char continuar;
+            printf("Continuar? S/N: ");
+            scanf(" %c", &continuar);
+            continuar = toupper(continuar);
+            if (continuar == 'S') {
+                menu = 0;
+            } else if (continuar == 'N') {
+                limparTela();
+                printf("Obrigado por jogar!\n");
+                menu = 2;
+            }
 
-    posnavioX = 3;
-    posnavioY = 4;
-
-    tabuleiro [posnavioX][posnavioY] = 3;
-    tabuleiro [posnavioX-1][posnavioY] = 3;
-    tabuleiro [posnavioX+1][posnavioY] = 3;
-
-
-    // posicionar o segundo navio
-
-    posnavioX = 6;
-    posnavioY = 2;
-
-    tabuleiro [posnavioX][posnavioY-1] = 3;
-    tabuleiro [posnavioX][posnavioY] = 3;
-    tabuleiro [posnavioX][posnavioY+1] = 3;
-
-    printf("\n----------------------------------\n");
-    impressaotabuleiro(tabuleiro, colunas);
-
+        break;
+        default:
+            limparTela();
+            printf("Obrigado por jogar!\n");
+        break;
+        }
+    } while (menu != 2);
     return 0;
 }
 
 // Imprime o tabuleiro atualizado
-void impressaotabuleiro(int tabuleiro[10][10], char colunas[10]) {
+void impressaotabuleiro(int tabuleiro[LINHAS][COLUNAS]) {
     printf("|  ");
-    for ( int i = 0; i < 10; i++) {
+    for ( int i = 0; i < COLUNAS; i++) {
         printf("|");
-        printf("%c", colunas[i]);
+        printf("%c", 'A' + i);
     }
 
     printf("|\n");
-    for ( int i = 0; i < 10 ; i++) {
+    for ( int i = 0; i < LINHAS ; i++) {
         if (i < 9){
             printf("| %i", i+1);
         } else {
             printf("|%i", i+1);
         }
-        for (int j = 0; j < 10; j++){
-            if (tabuleiro[j][i] == 0) {
+        for (int j = 0; j < COLUNAS; j++){
+            if (tabuleiro[i][j] == AGUA) {
                 printf("|~");
-            } else {
+            } else if (tabuleiro[i][j] == NAVIO) {
                 printf("|N");
+            } else {
+                printf("|X");
             }
         }
         printf("|\n");
+    }
+}
+
+//Posiciona os navios
+void shipdrop(int tabuleiro[LINHAS][COLUNAS]) {
+    char posXletra;
+    int posnavioX, posnavioY;
+    int shiprotation = 0; // 1 = Horizontal, 2 = Vertical, 3 = diagonal direita, 4 = diagonal esquerda
+
+
+    printf("\nEscolha o sentido que o navio será posicionado:\n\n1- Horizontal\n2- Vertical\n3- Diagonal direita\n4- Diagonal esquerda\n");
+    do { //Validação da opção
+        scanf("%i", &shiprotation);
+        if (shiprotation < 1 || shiprotation > 4 || shiprotation == 'A'){
+            printf("Entrada inválida!\nDigite novamente: ");
+        }
+        while (getchar() != '\n');
+    } while (shiprotation < 1 || shiprotation > 4 || shiprotation == 'A');
+    
+    printf("\nEscolha a Coluna para seu navio: ");
+    do { //Validação da opção
+        scanf("%c", &posXletra);
+        posXletra = toupper(posXletra);
+        posnavioX = posXletra - 'A';
+    
+        if (posnavioX < 0 || posnavioX> COLUNAS){
+            printf("Entrada inválida!\nDigite novamente: ");
+        }
+        while (getchar() != '\n');
+    } while (posnavioX < 0 || posnavioX> COLUNAS);
+    
+    printf("\nEscolha uma Linha para o navio: ");
+    do { //Validação da opção
+        scanf("%i", &posnavioY);
+        if (posnavioY < 0 || posnavioY> LINHAS){
+            printf("Entrada inválida!\nDigite novamente: ");
+        }
+        posnavioY--;
+    } while (posnavioY < 0 || posnavioY > LINHAS || posnavioY == 'A');
+    
+    //Correção de posição para o navio não sair da zona do tabuleiro
+    if (shiprotation !=2){
+        if (posnavioX == 0){
+            posnavioX += 1;
+        } else if (posnavioX >= COLUNAS -1){
+            posnavioX = COLUNAS-2;
+        }
+    }
+    if (shiprotation != 1){
+        if (posnavioY == 0){
+            posnavioY += 1;
+        } else if (posnavioY >= LINHAS -1){
+            posnavioY = LINHAS-2;
+        }
+    }
+
+    if (shiprotation == 1 ){
+            //Navio Horizontal
+        tabuleiro [posnavioY][posnavioX-1] = NAVIO;
+        tabuleiro [posnavioY][posnavioX] = NAVIO;
+        tabuleiro [posnavioY][posnavioX+1] = NAVIO;
+    } else if (shiprotation == 2){
+        //Navio Vertical
+        tabuleiro [posnavioY][posnavioX] = NAVIO;
+        tabuleiro [posnavioY-1][posnavioX] = NAVIO;
+        tabuleiro [posnavioY+1][posnavioX] = NAVIO;
+
+    }   else if (shiprotation == 3){
+        tabuleiro[posnavioY-1][posnavioX+1] = NAVIO;
+        tabuleiro[posnavioY][posnavioX] = NAVIO;
+        tabuleiro[posnavioY+1][posnavioX-1] = NAVIO;
+    }   else if (shiprotation == 4){
+        tabuleiro[posnavioY-1][posnavioX-1] = NAVIO;
+        tabuleiro[posnavioY][posnavioX] = NAVIO;
+        tabuleiro[posnavioY+1][posnavioX+1] = NAVIO;
+    }   else {
+        printf("Opção inválida\n\n");
     }
 }
